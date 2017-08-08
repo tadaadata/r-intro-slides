6a37eba9cea8ebb58d9d3077fe8693f2268ca23e
jemus42 <jemus42@quantenbrot.de>
2017-08-08 15:03:21 +0200

    Boy do I like building build scripts

--- build.R
+++ build.R
@@ -1,12 +1,17 @@
 #! /usr/bin/env Rscript
 
+t_start <- Sys.time()
+cat("\n\nRendering things…\n")
+
 rmarkdown::render(input         = "index.Rmd",
                   output_format = "revealjs::revealjs_presentation",
                   output_file   = "index.html")
 
 
 
+cat("\nChecking who you are…\n")
 current_user <- Sys.info()[["user"]]
+cat(paste0("I hope you're really ", current_user), "\n")
 
 if (current_user == "Lukas") {
   slides_dir <- "~/Sync/public.tadaa-data.de/r-intro/slides"
@@ -17,9 +22,15 @@ if (current_user == "Lukas") {
 }
 
 if (is.na(slides_dir)) {
-  message("No output directory defined, leaving everything as is.")
+  cat("No output directory defined, leaving everything as is.\n")
 } else {
   files <- c("assets", "index.html")
   file.copy(files, slides_dir, overwrite = TRUE, recursive = TRUE)
 }
 
+#### Done ####
+cat("\nAll done!\n")
+t_finish <- Sys.time()
+t_diff   <- round(as.numeric(difftime(t_finish, t_start, "s")), 0)
+cat("Took about", t_diff, "seconds", "\n")
+timestamp()
