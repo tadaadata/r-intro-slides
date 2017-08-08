04c2dfca44f55e305f751cb4c8a0d489dfa1a8d4
MarauderPixie <tobias.anton.85@gmail.com>
2017-08-08 14:22:54 +0200

    first steps; initialisation file & revealjs setup

---
+++ init.R
@@ -0,0 +1,8 @@
+## I like to keep things seperate...
+# ready toolbox
+library(tidyverse)
+library(tadaatoolbox)
+
+# load and set custom ggplot theme
+source("https://tobi.tadaa-data.de/theme_tut.R")
+theme_set(theme_tut(style = "light", size = 18))
