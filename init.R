fee8331cea138be6302830c4bb2d4e1dee1f2cd2
jemus42 <jemus42@quantenbrot.de>
2017-08-09 18:42:36 +0200

    Gitignore plot output

--- init.R
+++ init.R
@@ -6,3 +6,6 @@ library(tadaatoolbox)
 # load and set custom ggplot theme
 source("https://tobi.tadaa-data.de/theme_tut.R")
 theme_set(theme_tut(style = "light", size = 18))
+
+#### Chunk options ####
+knitr::opts_chunk$set(fig.path = "assets/plots/")
