252fbf6f9d32d3dcf7427c463333da09221625cf
jemus42 <jemus42@quantenbrot.de>
2017-08-11 20:55:54 +0200

    Tweaks

--- init.R
+++ init.R
@@ -1,7 +1,18 @@
 ## I like to keep things seperate...
+
+# Function to install packafe if needed and load afterwards
+pkg <- function(package) {
+  package <- deparse(substitute(package))
+
+  if (!(package %in% installed.packages())) {
+    install.packages(package)
+  }
+  library(package, character.only = TRUE)
+}
+
 # ready toolbox
-library(tidyverse)
-library(tadaatoolbox)
+pkg(tidyverse)
+pkg(tadaatoolbox)
 
 # load and set custom ggplot theme
 source("https://tobi.tadaa-data.de/theme_tut.R")
