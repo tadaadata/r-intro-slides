557cab27c7873a517d45599d787cceb1106d1322
jemus42 <jemus42@quantenbrot.de>
2017-08-11 23:05:04 +0200

    Tidier build script output

--- build.R
+++ build.R
@@ -5,9 +5,8 @@ cat("\n\nRendering things…\n")
 
 rmarkdown::render(input         = "index.Rmd",
                   output_format = "revealjs::revealjs_presentation",
-                  output_file   = "index.html", encoding = "UTF-8")
-
-
+                  output_file   = "index.html",
+                  encoding = "UTF-8", quiet = TRUE)
 
 cat("\nChecking who you are…\n")
 current_user <- Sys.info()[["user"]]
@@ -21,11 +20,19 @@ if (current_user == "Lukas") {
   slides_dir <- NA
 }
 
+cat("Pushing stuff to output directory…\n")
 if (is.na(slides_dir)) {
   cat("No output directory defined, leaving everything as is.\n")
 } else {
+  cat("Copying to", slides_dir)
   files <- c("assets", "index.html")
-  file.copy(files, slides_dir, overwrite = TRUE, recursive = TRUE)
+  status <- file.copy(files, slides_dir, overwrite = TRUE, recursive = TRUE)
+
+  if (all(status)) {
+    cat("\nCopying worked fine\n")
+  } else {
+    cat("Warning: Copying didn't work right!")
+  }
 }
 
 #### Done ####
