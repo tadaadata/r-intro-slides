e2f8f4832cd4bb3b1e8dff25f5eb4af6a328669e
jemus42 <jemus42@quantenbrot.de>
2017-11-06 16:48:47 +0100

    Code formatting

--- init.R
+++ init.R
@@ -26,13 +26,13 @@ knitr::opts_chunk$set(fig.path = "assets/plots/")
 
 # Via https://simplystatistics.org/2017/08/08/code-for-my-educational-gifs/
 ## simulate data
-N <- 100
+N     <- 100
 Sigma <- matrix(c(1,0.75,0.75, 1), 2, 2)*1.5
 means <- list(c(11,3), c(9,5), c(7,7), c(5,9), c(3,11))
-dat <- lapply(means, function(mu)
-  MASS::mvrnorm(N, mu, Sigma))
-dat <- tbl_df(Reduce(rbind, dat)) %>%
-  mutate(Z = as.character(rep(seq_along(means), each = N)))
+dat   <- lapply(means, function(mu)
+           MASS::mvrnorm(N, mu, Sigma))
+dat   <- tbl_df(Reduce(rbind, dat)) %>%
+           mutate(Z = as.character(rep(seq_along(means), each = N)))
 names(dat) <- c("X", "Y", "Z")
 
 ## First plot
@@ -55,4 +55,4 @@ sim_p3 <- sim_p2 +
   geom_smooth(method = lm, se = FALSE) +
   annotate("label", x = means$x, y = means$y, alpha = .6,
            label = paste("Z=", means$z), cex = 9) +
-  ggtitle(paste("Pearson's r = ",  paste(signif(corrs, 2), collapse=", ")))
+  ggtitle(paste("Pearson's r = ",  paste(signif(corrs, 2), collapse = ", ")))
