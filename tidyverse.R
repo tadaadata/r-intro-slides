a5084a23274cc8be5fc579d4b83e70b8580dfeb8
MarauderPixie <tobias.anton.85@gmail.com>
2017-11-14 16:36:10 +0100

    shorten on the one hand, further comment on the other

--- tidyverse.R
+++ tidyverse.R
@@ -7,11 +7,11 @@
 # kann, auf alten oder sehr leistungsschwachen Rechnern eine Weile dauern
 
 install.packages("ggplot2")         # Pakete müssen nur ein mal INSTALLIERT...
-install.packages("dplyr")
+install.packages("dplyr")           # das macht der Befehl/die Funktion install.packacges()
 install.packages("readr")
 
 library(ggplot2)                    # ...aber nach jedem Start von RStudio neu GELADEN werden
-library(dplyr)
+library(dplyr)                      # durch die Funktion library()
 library(readr)
 
 
@@ -32,50 +32,40 @@ got <- read_csv("data/gotdeaths_books.csv")
 
 
 
-#### Datenmanipulation ####
-
-got <- got %>%
-  rename(
-    Loyalität = Allegiances, Todesjahr = `Death Year`, Todesbuch = `Book of Death`,
-    Todeskapitel = `Death Chapter`, Vorstellungskapitel = `Book Intro Chapter`, Geschlecht = Gender,
-    Adel = Nobility, Game_of_Thrones = GoT, Clash_of_Kings = CoK, Storm_of_Swords = SoS, Feast_for_Crows = FfC,
-    Dance_with_Dragons = DwD
-  ) %>%
-  mutate(
-    Loyalität  = stringr::str_replace_all(Loyalität, "House\\ ", ""),
-    Geschlecht = factor(recode(Geschlecht, `1` = "Männlich", `0` = "Weiblich")),
-    Adel       = factor(recode(Adel, `1` = "Ja", `0` = "Nein"))
-  )
-
-
-
 #### Ein paar hübsche Plots bauen ####
 
 # Tode in den ASOIAF-Büchern nach Häusern (u.a.)
-got %>%
-  filter(!is.na(Todesjahr)) %>%
-  count(Loyalität) %>%
-  ggplot(aes(x = reorder(Loyalität, n), y = n)) +
-    geom_col() +
+got %>%                               # nimm das Objekt: got
+  filter(!is.na('Death Year')) %>%    # entferne alle Zeilen, die in der Spalte "Death Year"
+                                      # nicht NA sind (siehe Boolesche Logik)
+  count(Allegiances) %>%              # erstelle eine Häufigkeitsübersicht ("zähle") der Einträge
+                                      # der Variable "Allegiances"
+  ggplot(aes(x = reorder(Allegiances, n), y = n)) + # Erstelle eine Grafik mit der nach Häufigkeit
+                                                    # sortierten Variable "Allegiances" auf der x-Achse
+                                                    # und der Häufigkeit auf der y-Achse
+    geom_col() +                      # die Grafik soll ein Balkendiagramm sein
     labs(y = "Tode", x = "Loyalität", title = "Tode in Game of Thrones",
-         subtitle = "basierend auf den Büchern") +
-    coord_flip()
+         subtitle = "basierend auf den Büchern") +  # beschrifte die Achsen
+    coord_flip()                      # und drehe die Grafik (sodass die Beschriftung lesbar ist)
 
 
 # Anzahl Sexualpartnerschaften nach Parteipräferenz
+qm %>%                                                        # nimm den Datensatz qm
+  filter(partnerinnen <= 15, !is.na(partei)) %>%              # behalte alle Werte, auf die Filter zutreffen
+  ggplot(aes(x = partei, y = partnerinnen, fill = partei)) +  # erstelle eine Grafik; 'fill' färbt Daten ein
+    geom_boxplot() +                                          # die Grafik soll ein Bocplot sein
+    labs(x = "Partei", y = "Anzahl Sexualpartnerschaften") +  # beschrifte die Achsen
+    coord_flip() +                                            # drehe die Grafik
+    theme(legend.position = "none")                           # und blende die Legende aus
+
+
+### Hinweise:
+# bei den Grafiken könnt ihr gerne jede Zeile nacheinander ausführen und schauen, was raus kommt, zB:
+qm %>%
+  filter(partnerinnen <= 15, !is.na(partei))  # es bleiben dann nur noch 91 Beobachtungen übrig
+
+# oder
 qm %>%
   filter(partnerinnen <= 15, !is.na(partei)) %>%
   ggplot(aes(x = partei, y = partnerinnen, fill = partei)) +
-    geom_boxplot() +
-    scale_color_brewer(palette = "Set1") +
-    labs(x = "Partei", y = "Anzahl Sexualpartnerschaften") +
-    coord_flip() +
-    theme(legend.position = "none")
-
-
-# Freunde & so
-qm %>%
-  filter(freundeeng <= 15, freundeskreis <= 100) %>%
-  ggplot(aes(x = freundeskreis, y = freundeeng)) +
-  geom_point(shape = 21) +
-  geom_smooth(method = "lm", se = F)
+    geom_boxplot() # gibt auch eine Grafik aus, aber es fehlen Details
